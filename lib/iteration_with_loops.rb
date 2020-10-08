5141df9170c71c6d314e30123fa993c0b94ac5f2
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-08 19:18:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,8 @@
 def join_nested_strings(src)
-  
+  result = ""
+  row_index = 0 
+  while row_index < src.count do
+    element_index = 0 
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].
 end
